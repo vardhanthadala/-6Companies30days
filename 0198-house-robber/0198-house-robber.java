002beeeeb45235e31aff1c78e40b815e1592a32d
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-04-19 19:31:31 +0530

    Time: 0 ms (100%), Space: 41.2 MB (46.5%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -41,19 +41,37 @@ class Solution {
     // }
 
     //Tabulation O(N) O(N)
-    public int rob(int[] nums) {
-        int n = nums.length;
-        if (n == 0)
-            return 0;
-        if (n == 1)
-            return nums[0];
+    // public int rob(int[] nums) {
+    //     int n = nums.length;
+    //     if (n == 0)
+    //         return 0;
+    //     if (n == 1)
+    //         return nums[0];
 
-        int dp[] = new int[n];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-        for (int i = 2; i < n; i++) {
-            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+    //     int dp[] = new int[n];
+    //     dp[0] = nums[0];
+    //     dp[1] = Math.max(nums[0], nums[1]);
+    //     for (int i = 2; i < n; i++) {
+    //         dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+    //     }
+    //     return dp[n - 1];
+    // }
+
+    //space optimization O(N) O(1)
+    public int rob(int[] nums) {
+        int prev1 = nums[0];
+        int prev2 = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            int pick = nums[i];
+            if (i > 1) {
+                pick += prev2;
+            }
+            int nonPick=prev1;
+            int curr_i=Math.max(pick,nonPick);
+            prev2=prev1;
+            prev1=curr_i;
         }
-        return dp[n - 1];
+        return prev1;
     }
 }
