6c37bf761eb406e7a33fb3266ef3ccec2fe855cb
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-04-19 19:24:43 +0530

    Time: 0 ms (100%), Space: 41.7 MB (8.61%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,5 +1,5 @@
 class Solution {
-    //Recursive approach
+    //Recursive approach O(2^n)
     // public int rob(int[] nums) {
     //     int n = nums.length;
     //     return f(n - 1, nums);
@@ -17,26 +17,43 @@ class Solution {
     //     return Math.max(pick, notPick);
     // }
 
-    //Memoization O(2^n)
+    //Memoization  O(N) O(N)
+    // public int rob(int[] nums) {
+    //     int n = nums.length;
+    //     int dp[] = new int[n];
+    //     Arrays.fill(dp, -1);
+
+    //     return f(n - 1, nums, dp);
+    // }
+
+    // int f(int idx, int[] nums, int dp[]) {
+    //     if (idx == 0)
+    //         return nums[idx];
+    //     if (idx < 0)
+    //         return 0;
+
+    //         if(dp[idx]!=-1) return dp[idx];
+
+    //     int pick = nums[idx] + f(idx - 2, nums,dp);
+    //     int notPick = 0 + f(idx - 1, nums,dp);
+
+    //     return dp[idx]=Math.max(pick, notPick);
+    // }
+
+    //Tabulation O(N) O(N)
     public int rob(int[] nums) {
         int n = nums.length;
-        int dp[] = new int[n];
-        Arrays.fill(dp, -1);
-
-        return f(n - 1, nums, dp);
-    }
-
-    int f(int idx, int[] nums, int dp[]) {
-        if (idx == 0)
-            return nums[idx];
-        if (idx < 0)
+        if (n == 0)
             return 0;
+        if (n == 1)
+            return nums[0];
 
-            if(dp[idx]!=-1) return dp[idx];
-
-        int pick = nums[idx] + f(idx - 2, nums,dp);
-        int notPick = 0 + f(idx - 1, nums,dp);
-
-        return dp[idx]=Math.max(pick, notPick);
+        int dp[] = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+        }
+        return dp[n - 1];
     }
 }
