eada6d5a25ed5e2500189240970d8aed1f1d3bfa
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-05-10 11:44:36 +0530

    Time: 266 ms (43.59%), Space: 42.2 MB (88.74%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,16 +1,24 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
-int minRepeats=(int) Math.ceil((double) b.length() / a.length());        StringBuilder sb = new StringBuilder(a.repeat(minRepeats));  
-        
-        if (sb.toString().contains(b)) {
-            return minRepeats;
+        StringBuilder repeated = new StringBuilder();
+        int count = 0;
+
+        while (repeated.length() < b.length()) {
+            repeated.append(a);
+            count++;
         }
-        
-        sb.append(a);
-        if (sb.toString().contains(b)) {
-            return minRepeats + 1;
+
+        if (repeated.indexOf(b) != -1) {
+            return count;
         }
-        
-        return -1; 
+
+        repeated.append(a);
+        count++;
+
+        if (repeated.indexOf(b) != -1) {
+            return count;
+        }
+
+        return -1;
     }
 }
