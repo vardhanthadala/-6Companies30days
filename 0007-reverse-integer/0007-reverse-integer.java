6e0caa425d02254c45fe5f11d9d7d5f31f01518b
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-05-15 11:19:19 +0530

    Time: 1 ms (83.9%), Space: 41.3 MB (16.11%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -3,12 +3,13 @@ class Solution {
         int reversed =0;
         while(x!=0){
             int digit=x%10; //gets last digit
-            x/=10;//removes last digit for next process
+            
 
             if(reversed > Integer.MAX_VALUE / 10 || (reversed == Integer.MAX_VALUE / 10 && digit > 7)) return 0; // check for overflow
             if (reversed < Integer.MIN_VALUE / 10 || (reversed == Integer.MIN_VALUE / 10 && digit < -8)) return 0;//check for overflow
 
             reversed=reversed*10+digit; //adding digts by multiplying last digit with 10
+            x/=10;//removes last digit for next process
         }
         return reversed;
     }
