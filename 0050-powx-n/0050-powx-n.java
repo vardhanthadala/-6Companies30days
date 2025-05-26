388dc46412e0ff5a84534dbc335670d3f11d8c14
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-05-26 23:07:03 +0530

    Time: 0 ms (100%), Space: 41.9 MB (80.84%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -11,31 +11,53 @@ class Solution {
     // return ans;
     // }
 
-    //binary exponential : O(logn)  space : O(1)
+    //     public double myPow(double x, int n) { //binary exponential : O(logn)  space : O(1)  
 
-  
-    public double myPow(double x, int n) {
+    //    //  for even n = (x*x) power (n/2) 
+    //   //for odd n = (x*x) power  (n-1)
 
-   //  for even n = (x*x) power (n/2) 
-  //for odd n = (x*x) power  (n-1)
+    //         long power = n; // Use long 
+    //         if (power < 0) {
+    //             x = 1 / x;
+    //             power = -power;
+    //         }
 
-        long power = n; // Use long to handle n = Integer.MIN_VALUE safely
-        if (power < 0) {
+    //         double result = 1.0;
+
+    //         while (power > 0) {
+    //             if ((power % 2) == 1) {//odd
+    //                 result *= x;
+    //             }
+    //             x *= x;
+    //             power /= 2;
+    //         }
+
+    //         return result;
+
+    //     }
+
+    public double myPow(double x, int n) {//recursive approach
+
+        if (n == 0) {
+            return 1;
+        }
+        // Convert n to a long integer to handle the edge case with Integer.MIN_VALUE
+        long N = n;
+
+        // If n is negative, take the reciprocal of x and make N positive
+        if (N < 0) {
+            N = -N;
             x = 1 / x;
-            power = -power;
+
+        }
+        // If N is even, recursively compute the square of x^(N/2)
+        if (N % 2 == 0) {
+            return myPow(x * x, (int) (N / 2));
         }
 
-        double result = 1.0;
-
-        while (power > 0) {
-            if ((power % 2) == 1) {//odd
-                result *= x;
-            }
-            x *= x;
-            power /= 2;
+        // If N is odd, recursively compute x^(N-1) and multiply it by x
+        else {
+            return x * myPow(x, (int) (N - 1));
         }
-
-        return result;
-
     }
 }
