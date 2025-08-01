f2f1bb9a698cc5eb4bb0254c70626ee3bdfa30a1
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-08-02 00:37:23 +0530

    Time: 11 ms (67.48%), Space: 42.6 MB (52.26%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,19 +1,44 @@
 class Solution {
-    public boolean isIsomorphic(String s, String t) {
-        int[] indexS = new int[200];
-        int[] indexT = new int[200];
+    public boolean isIsomorphic(String s, String t) {//O(n) O(n)
+        HashMap<Character, Character> map = new HashMap<>();
+        Set<Character> set = new HashSet<>();
 
-        if (s.length() != t.length()) {
-            return false;
-        }
         for (int i = 0; i < s.length(); i++) {
-            if (indexS[s.charAt(i)] != indexT[t.charAt(i)] ){//current inndex of s is different from t
-                return false;
-            }
+            char s2 = s.charAt(i);
+            char t2 = t.charAt(i);
+            if (map.containsKey(s2)) {
+                // Check if existing mapping is consistent
+                if (map.get(s2) != t2)
+                    return false;
+            } else {
+                // Check if t2 is already used for another char
+                if (set.contains(t2))
+                    return false;
 
-            indexS[s.charAt(i)] = i + 1;//update index of current character
-            indexT[t.charAt(i)] = i + 1;//update index of current character
+                map.put(s2, t2); // Create new mapping
+                set.add(t2); // Mark ct as used
+            }
         }
+
         return true;
     }
+    
+
+    // public boolean isIsomorphic(String s, String t) {
+    //     int[] indexS = new int[200];
+    //     int[] indexT = new int[200];
+
+    //     if (s.length() != t.length()) {
+    //         return false;
+    //     }
+    //     for (int i = 0; i < s.length(); i++) {
+    //         if (indexS[s.charAt(i)] != indexT[t.charAt(i)] ){//current inndex of s is different from t
+    //             return false;
+    //         }
+
+    //         indexS[s.charAt(i)] = i + 1;//update index of current character
+    //         indexT[t.charAt(i)] = i + 1;//update index of current character
+    //     }
+    //     return true;
+    // }
 }
