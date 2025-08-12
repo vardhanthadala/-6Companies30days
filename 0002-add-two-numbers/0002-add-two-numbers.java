25ba104b8ca3bb340aa47a372e1b8797de7c6afd
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-08-12 22:05:24 +0530

    Time: 1 ms (100%), Space: 44.4 MB (68.18%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,36 +1,32 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode();// time:O(max(m,n)) space:O(n) for creating new ll
-        ListNode temp = dummy;
-
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
         int carry = 0;
 
-        while (l1 != null || l2 != null || carry == 1) {
-            int sum = 0;
-            if (l1 != null) {//adding all vals in l1
-                sum += l1.val;
-                l1 = l1.next;
-            }
-            if (l2 != null) {//adding all vals in l2
-                sum += l2.val;
-                l2 = l2.next;
-            }
-            sum += carry;//adding any leftover carry from the previous addition
-            carry = sum / 10;//calculating new carry
-            ListNode node = new ListNode(sum % 10);//Creating a new node for the ones digit of the sum
-            temp.next = node; //new node to prev node (addition is from back to front)
-            temp = temp.next;
+        while (l1 != null || l2 != null || carry != 0) {
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
+
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;  
+            curr.next = new ListNode(sum % 10); // new digit
+
+            curr = curr.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
         }
+
         return dummy.next;
     }
 }
