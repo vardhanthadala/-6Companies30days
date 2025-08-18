a2c1c1384e383dd96e8053f2032ae58ea27a1ae6
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-08-18 21:43:54 +0530

    Time: 25 ms (27.97%), Space: 45.6 MB (80.8%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -0,0 +1,80 @@
+class Solution {
+    //similar to Largest rectangle in histogram 
+    public int maximalRectangle(char[][] matrix) { //T : O(M*N) +O(N+2M)  S:O(M*N)+O(N)
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] heights = new int[n]; // Histogram representation
+        int maxArea = 0;
+
+        // Iterate over each row to build the histogram
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    heights[j] += 1;
+                } else {
+                    heights[j] = 0;
+                }
+            }
+            maxArea = Math.max(maxArea, largestRectangleArea(heights));
+        }
+
+        return maxArea;
+    }
+
+    private int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+
+        int n = heights.length;
+        int[] nsl = new int[n];
+        int[] nsr = new int[n];
+
+        for (int i = 0; i < n; i++) {//next smaller left element
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
+            }
+            nsl[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+
+        stack.clear();
+
+        for (int i = n - 1; i >= 0; i--) { //next smaller right element
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
+            }
+            nsr[i] = stack.isEmpty() ? n : stack.peek(); //n not -1
+            stack.push(i);
+        }
+
+        for (int i = 0; i < n; i++) {
+            int height = heights[i];
+            int width = nsr[i] - nsl[i] - 1;
+            int currArea = height * width;
+            maxArea = Math.max(maxArea, currArea);
+        }
+
+        return maxArea;
+    }
+}
+
+/*
+         -----------------------
+         | 1 |  0 | 1 | 0 | 1  |                             
+         | 1 |  0 | 1 | 1 | 1  | 
+         | 1 |  1 | 1 | 1 | 1  |                        
+         | 1 |  0 | 0 | 1 | 0  |                           
+         -----------------------  
+
+                    ↓
+         -----------------------
+         | 1 |  0 | 1 | 0 | 1  |
+         | 2 |  0 | 2 | 1 | 2  | 
+         | 3 |  1 | 3 | 2 | 3  |
+         | 4 |  0 | 0 | 3 | 0  |
+         -----------------------                          
+*/
