9e28f4736ac7650c87f232560f801a35a68fadbb
Vardhan <73521043+vardhanthadala@users.noreply.github.com>
2025-08-21 22:07:37 +0530

    Time: 32 ms (38.42%), Space: 68.1 MB (15.95%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -0,0 +1,62 @@
+class Solution {
+    //brute force using linearsearch O(N^2) O(1)
+
+    // private static boolean linearSearch(int[] a, int k) {
+    //     int n = a.length; // size of array
+    //     for (int i = 0; i < n; i++) {
+    //         if (a[i] == num)
+    //             return true;
+    //     }
+    //     return false;
+    // }
+
+    // public int longestConsecutive(int[] nums) {
+    //     int n = nums.length;
+    //     int longest = 1;
+
+    //     for (int i = 0; i < n; i++) {
+    //         int x = nums[i];
+    //         int cnt = 1;
+
+    //         while (linearSearch(nums, x + 1) == true) {
+    //             x++;
+    //             cnt++;
+    //         }
+
+    //         longest = Math.max(longest, cnt);
+    //     }
+    //     return longest;
+    // }
+
+    // optimal approach using HashSet O(2*N) O(N)
+
+    public int longestConsecutive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        HashSet<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num); // add elements to numSet
+        }
+
+        int longestStreak = 0;
+
+        for (int num : numSet) {
+
+            if (!numSet.contains(num - 1)) {
+
+                int currentStreak = 1;
+
+                while (numSet.contains(num + 1)) {
+                    num++;
+                    currentStreak++;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+}
